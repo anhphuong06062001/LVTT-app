23906d0a47925953b44dd4d304cd8f1cd64ee13f
anhphuong06062001 <anhphuong06062001@gmail.com>
2023-11-24 17:09:24 +0700

    update code

--- main.py
+++ main.py
@@ -83,12 +83,26 @@ st.set_page_config(layout="wide")
 if 'df' not in st.session_state:
     st.session_state.df = None
 
+if 'id_col' not in st.session_state:
+    st.session_state.id_col = None
+
+if 'custumer' not in st.session_state:
+    st.session_state.custumer = None
+
+if 'lable' not in st.session_state:
+    st.session_state.lable = None
+
+if 'date' not in st.session_state:
+    st.session_state.date = None
+
+if 'chart' not in st.session_state:
+    st.session_state.chart = None
 
 with st.sidebar:
       selected = option_menu(
             menu_title = "Main Menu",
             options = ['Home', 'Import Data', 'Draw Chart', 'Prediction', 'Feedback'],
-            icons=["house", "cloud-upload", "envelope", "list-task", "people-fill"],
+            icons=["house", "cloud-upload", "bar-chart-fill", "list-task", "people-fill"],
             menu_icon="cast",
       )
 
@@ -113,74 +127,85 @@ elif selected == 'Import Data':
       # session_state.page = 'Import Data'
       st.title('Import Data')
       uploaded_file = st.file_uploader("Upload file CSV or Excel", type=['csv', 'xlsx'])
-      #st.session_state.show = ''      
-      if uploaded_file is not None:
-            st.session_state.df = pd.read_csv(uploaded_file)  
-            
-            if st.session_state.df is not None and not st.session_state.df.empty:
-                  columns = st.session_state.df.columns.tolist()
-                  id_col = st.selectbox("Select data for id custumer", columns)
-                  unique_customers = st.session_state.df[id_col].unique()  
-                  lable_col = st.selectbox("Select data for lable", columns)
-                  date_col = st.selectbox("Select data for date", columns)
-                  if st.button("Draw"):
-                      draw.show_data_info(st.session_state.df, unique_customers ,lable_col, id_col, date_col)
+      #st.session_state.show = ''   
+      with st.form(key='import_file', clear_on_submit=False):
+            if uploaded_file is not None:
+                  st.session_state.df = pd.read_csv(uploaded_file)  
+                  
+                  if st.session_state.df is not None and not st.session_state.df.empty:
+                        columns = st.session_state.df.columns.tolist()
+                        id_col = st.selectbox("Select data for id custumer", columns)
+                        unique_customers = st.session_state.df[id_col].unique()  
+                        lable_col = st.selectbox("Select data for lable", columns)
+                        date_col = st.selectbox("Select data for date", columns)
+                        if st.form_submit_button("Draw"):
+                              draw.show_data_info(st.session_state.df, unique_customers ,lable_col, id_col, date_col)
                   #     st.session_state.show = kq
       
-      
-      # st.write(st.session_state.show)
+            else:
+                  if st.session_state.df is not None and not st.session_state.df.empty:
+                        columns = st.session_state.df.columns.tolist()
+                        st.session_state.id_col = st.selectbox("Select data for id custumer", columns)
+                        st.session_state.custumer = st.session_state.df[st.session_state.id_col].unique()  
+                        st.session_state.lable = st.selectbox("Select data for lable", columns)
+                        st.session_state.date = st.selectbox("Select data for date", columns)
+                        if st.form_submit_button("Draw"):
+                              st.session_state.chart = draw.show_data_info(st.session_state.df, st.session_state.custumer ,st.session_state.lable, st.session_state.id_col, st.session_state.date)
+      # st.write(st.session_state.chart)
                   
 elif selected == 'Draw Chart':
       # session_state.page = 'Draw Chart'
       st.title('Draw Chart')
-      if st.session_state.df is not None and not st.session_state.df.empty:
-            columns = st.session_state.df.columns.tolist()
+      with st.form(key="draw_chart"):
+            if st.session_state.df is not None and not st.session_state.df.empty:
+                  columns = st.session_state.df.columns.tolist()
 
-            x_axis = st.selectbox("Select data for X axis", columns)
-            y_axis = st.selectbox("Select data for Y axis", columns)
+                  x_axis = st.selectbox("Select data for X axis", columns)
+                  y_axis = st.selectbox("Select data for Y axis", columns)
 
-            customer_id_column = st.selectbox("Select column for Customer ID", columns)
+                  customer_id_column = st.selectbox("Select column for Customer ID", columns)
 
-            unique_customers = st.session_state.df[customer_id_column].unique()  
-            num_customers = st.slider("Select number of customers", min_value=1, max_value=len(unique_customers), value=10)
+                  unique_customers = st.session_state.df[customer_id_column].unique()  
+                  num_customers = st.slider("Select number of customers", min_value=1, max_value=len(unique_customers), value=10)
 
-            if st.button("Draw Chart"):
-                  selected_customers = unique_customers[:num_customers]
+                  if st.form_submit_button("Draw Chart"):
+                        selected_customers = unique_customers[:num_customers]
 
-                  
-                  filtered_data = st.session_state.df[st.session_state.df[customer_id_column].isin(selected_customers)]
+                        
+                        filtered_data = st.session_state.df[st.session_state.df[customer_id_column].isin(selected_customers)]
 
-                  final_data = pd.DataFrame()
-                  for customer_id in selected_customers:
-                        final_data = pd.concat([final_data, filtered_data[filtered_data[customer_id_column] == customer_id]])
-                  st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
-                  draw.plot_customer_data(final_data, x_axis, y_axis, customer_id_column)
-                  st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
-                  draw.trend_month(final_data, x_axis, y_axis)
+                        final_data = pd.DataFrame()
+                        for customer_id in selected_customers:
+                              final_data = pd.concat([final_data, filtered_data[filtered_data[customer_id_column] == customer_id]])
+                        st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
+                        draw.plot_customer_data(final_data, x_axis, y_axis, customer_id_column)
+                        st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
+                        draw.trend_month(final_data, x_axis, y_axis)
             
 
 elif selected == 'Prediction':
       # session_state.page = 'Prediction'
       st.title('Prediction')
-      if st.session_state.df is not None and not st.session_state.df.empty:
-            columns = st.session_state.df.columns.tolist()
+      with st.form(key="prediction"):
+            if st.session_state.df is not None and not st.session_state.df.empty:
+                  columns = st.session_state.df.columns.tolist()
 
-            data_column = st.selectbox("Select data for predict", columns)
-            customer_id_column = st.selectbox("Select column for Customer ID", columns)
-            date_column = st.selectbox("Select data for time serise", columns)
-            selected_model = st.selectbox("Select Machine Learning Model", ["LSTM for Upload", "LSTM for Download"])
+                  data_column = st.selectbox("Select data for predict", columns)
+                  customer_id_column = st.selectbox("Select column for Customer ID", columns)
+                  date_column = st.selectbox("Select data for time serise", columns)
+                  selected_model = st.selectbox("Select Machine Learning Model", ["LSTM for Upload", "LSTM for Download"])
 
-            if st.button('Predict'):
-                  st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
-                  X_test, y_test, id_test, ngay_test = create_time_series_data(st.session_state.df, data_column, customer_id_column, date_column, 23, 7)
-                  X_test_up = np.reshape(X_test, (X_test.shape[0],X_test.shape[1], 1))
-                  
-                  if selected_model == "LSTM for Upload":
-                        kq = predict_churn_up(X_test_up, y_test, id_test, ngay_test)
-                        st.write(kq)
-                  elif selected_model == "LSTM for Download":
-                        kq = predict_churn_down(X_test_up, y_test, id_test, ngay_test)
-                        st.write(kq)
+                  if st.form_submit_button('Predict'):
+                        st.markdown('<hr style="border:1px solid #F63366;">', unsafe_allow_html=True)
+                        X_test, y_test, id_test, ngay_test = create_time_series_data(st.session_state.df, data_column, customer_id_column, date_column, 23, 7)
+                        X_test_up = np.reshape(X_test, (X_test.shape[0],X_test.shape[1], 1))
+                        
+                        if selected_model == "LSTM for Upload":
+                              kq = predict_churn_up(X_test_up, y_test, id_test, ngay_test)
+                              st.write(kq)
+                        elif selected_model == "LSTM for Download":
+                              kq = predict_churn_down(X_test_up, y_test, id_test, ngay_test)
+                              st.write(kq)
 
 elif selected == 'Feedback':
       st.header(":mailbox: Please give me your comments to help me improve the application!!!")
